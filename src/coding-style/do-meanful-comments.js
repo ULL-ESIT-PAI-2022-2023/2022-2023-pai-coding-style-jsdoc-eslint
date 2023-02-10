9d1387b3e0d608d8c2aab76a84e51a0698adf2a6
alu0101127163 <38443567+alu0101127163@users.noreply.github.com>
2023-02-10 14:01:40 +0000

    Create do-meanful-comments.js

---
+++ src/coding-style/do-meanful-comments.js
@@ -0,0 +1,14 @@
+let value = 5; // set value to 5
+let string = 'Some string'; // set string to 'Some string'
+
+for (char for string) { // iterates through the string
+  statement;
+}
+
+// increase the value of count
+++count;
+
+// add two numbers
+function sumTwoNumbers(addend1, addend2) {
+  return addend1 + addend2; // sum of two numbers
+}
