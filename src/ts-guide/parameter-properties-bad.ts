28e111076b9289aa7af5aa7c0206a60576512862
alu0101127163 <38443567+alu0101127163@users.noreply.github.com>
2023-02-12 18:18:25 +0000

    Create parameter-properties-bad.ts

---
+++ src/ts-guide/parameter-properties-bad.ts
@@ -0,0 +1,11 @@
+class Coordinates {
+  public readonly x: number;
+  protected y: number;
+  private z: number;
+  
+  constructor(x: number, y: number, z: number) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
