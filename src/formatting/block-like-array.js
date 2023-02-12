b3c587c38f7070fa9b4e28d08a05d5307c0b6357
Alicia G. Cruz Pérez <alu0101420868@ull.edu.es>
2023-02-12 02:38:43 +0000

    Add files via upload

---
+++ src/formatting/block-like-array.js
@@ -0,0 +1,14 @@
+const NUMBERS = [
+  0,
+  1,
+  2,
+];
+
+const ANIMALS =
+    ['dog', 'cat', 'bee'];
+
+const COLORS = ['red', 'pink', 'blue'];
+
+someMethod(foo, [
+  0, 1, 2,
+], bar);
---
+++ src/formatting/block-like-object.js
@@ -0,0 +1,14 @@
+const NUMBERS = {
+  zero: 0,
+  one: 1,
+  two: 2,
+};
+
+const ANIMALS =
+    {dog: 'dog', cat: 'cat', bee: 'bee'};
+
+const COLORS = {red: 'red', pink: 'pink', blue: 'blue'};
+
+someMethod(foo, {
+  zero: 0, one: 1, two: 2,
+}, bar);
