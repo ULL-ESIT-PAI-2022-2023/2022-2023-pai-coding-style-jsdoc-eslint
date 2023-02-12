38ce4e91a1a6d7caef24535214fc270e7e75a887
alu0101127163 <38443567+alu0101127163@users.noreply.github.com>
2023-02-12 02:16:04 +0000

    Create default-parameters.js

---
+++ src/coding-style/default-parameters.js
@@ -0,0 +1,9 @@
+function multiply(multiplicand, multiplier = 1) {
+  return multiplicand * multiplier;
+}
+
+console.log(multiply(5, 2));
+// Expected output: 10
+
+console.log(multiply(5));
+// Expected output: 5
